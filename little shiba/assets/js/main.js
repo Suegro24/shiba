9f0576e9f30f9b5aa20031175a1c434d522a2349
Suegro24 <marciu199@gmail.com>
2021-08-08 01:58:41 +0200

    Dropdown list can now close

--- little shiba/assets/js/main.js
+++ little shiba/assets/js/main.js
@@ -10,22 +10,32 @@ const list = document.querySelector('#dropDownList');
 const listItems = list.querySelectorAll('.drop-down-list__list-item');
 
 const changeActiveList = (e) => {
+    const newListNumber = e.target.parentNode.parentNode.dataset['number'];
+    let oldListNumber = 0;
+
     listItems.forEach(item => {
         const header = item.querySelector('.header');
         const internalList = item.querySelector('.internal-list');
         const image = item.querySelector('img');
 
+        if (header.classList.contains('header--active')) {
+            oldListNumber = item.dataset['number'];
+        }
+
         header.classList.remove('header--active');
         internalList.classList.remove('internal-list-active')
         image.src="./assets/images/arrow-down-black.svg"
     })
-    const header = e.target.parentNode.parentNode.querySelector('.header');
-    const internalList = e.target.parentNode.parentNode.querySelector('.internal-list');
-    const image = e.target.parentNode.parentNode.querySelector('img');
 
-    header.classList.add('header--active');
-    internalList.classList.add('internal-list-active')
-    image.src="./assets/images/arrow-down-blue.svg"    
+    if (oldListNumber !== newListNumber) {
+        const header = e.target.parentNode.parentNode.querySelector('.header');
+        const internalList = e.target.parentNode.parentNode.querySelector('.internal-list');
+        const image = e.target.parentNode.parentNode.querySelector('img');
+
+        header.classList.add('header--active');
+        internalList.classList.add('internal-list-active')
+        image.src="./assets/images/arrow-down-blue.svg" 
+    }
 }
 
 listItems.forEach(item => {
